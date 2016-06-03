264b6a6d84ff9c2c3f5ab0758be46f9d80a189b4
tczf <tczf@MBA.local>
2016-06-04 00:03:06 +0800

    Add chat sample

---
+++ client.ex
@@ -0,0 +1,28 @@
+defmodule Client do
+
+  def connect(username, server) do
+    spawn(__MODULE__, :init, [username, server])
+  end
+
+  def init(username, server) do
+    send server, {self, :connect, username}
+    loop(username, server)
+  end
+
+  def loop(username, server) do
+    receive do
+      {:info, msg} ->
+        IO.puts(~s{[#{username}'s client] - #{msg}})
+        loop(username, server)
+      {:new_msg, from, msg} ->
+        IO.puts(~s{[#{username}'s client] - #{from}: #{msg}})
+        loop(username, server)
+      {:send, msg} ->
+        send server, {self, :broadcast, msg}
+        loop(username, server)
+      :disconnect ->
+        exit(0)
+    end
+  end
+end
+
